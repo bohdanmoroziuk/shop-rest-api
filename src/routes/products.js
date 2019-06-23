c94755b4af90957a71b4131c518a9fb687e7b8fc
Bogdan <bogdan7morozyuk@gmail.com>
2019-06-23 17:25:34 +0300

    Add request processing for products

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,33 +1,85 @@
 const router = require('express').Router();
+const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'Handling GET request /products'
-  });
+  Product.find()
+    .then(products => {
+      res.status(200).json({
+        message: 'Handling GET request /products',
+        products
+      });
+    })
+    .catch(error => {
+      res.status(404).json({
+        error
+      });
+    });
 });
 
 router.get('/:id', (req, res, next) => {
-  res.status(200).json({
-    message: 'Handling GET request /products/:id'
-  });
+  Product.findById(req.params.id)
+    .then(product => {
+      res.status(200).json({
+        message: 'Handling GET request /products/:id',
+        product
+      });
+    })
+    .catch(error => {
+      res.status(404).json({
+        error
+      });
+    });
 });
 
 router.post('/', (req, res, next) => {
-  res.status(201).json({
-    message: 'Handling POST request /products'
-  });
+  const { name, price } = req.body;
+
+  new Product({
+    name,
+    price
+  })
+    .save()
+    .then(product => {
+      res.status(201).json({
+        message: 'Handling POST request /products',
+        createdProduct: product
+      });
+    })
+    .catch(error => {
+      res.status(500).json({
+        error
+      });
+    });
 });
 
 router.patch('/:id', (req, res, next) => {
-  res.status(200).json({
-    message: 'Handling PATCH request /products/:id'
-  });
+  Product.findByIdAndUpdate(req.params.id, { $set: { ...req.body } })
+    .then(product => {
+      res.status(201).json({
+        message: 'Handling PATCH request /products/:id',
+        createdProduct: product
+      });
+    })
+    .catch(error => {
+      res.status(500).json({
+        error
+      });
+    });
 });
 
 router.delete('/:id', (req, res, next) => {
-  res.status(200).json({
-    message: 'Handling DELETE request /products/:id'
-  });
+  Product.findByIdAndRemove(req.params.id)
+    .then(product => {
+      res.status(200).json({
+        message: 'Handling DELETE request /products/:id',
+        deletedProduct: product
+      });
+    })
+    .catch(error => {
+      res.status(500).json({
+        error
+      });
+    });
 });
 
 module.exports = router;
