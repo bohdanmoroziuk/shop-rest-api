40fc9f504f55dcdce135c6bb5c7562cc765a7649
Bogdan <bogdan7morozyuk@gmail.com>
2019-06-23 16:13:13 +0300

    Handle CORS

--- app.js
+++ app.js
@@ -11,6 +11,22 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, GET, DELETE');
+
+    return res.status(200).json({});
+  }
+
+  next();
+});
+
 app.use('/products', productsRoutes);
 app.use('/orders', ordersRoutes);
 
