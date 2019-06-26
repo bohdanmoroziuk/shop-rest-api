89da0e7548f80d488973919b06c802717f1ec590
Bogdan <bogdan7morozyuk@gmail.com>
2019-06-26 21:23:34 +0300

    Add the ability to upload images for the product

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,10 +1,40 @@
 const router = require('express').Router();
+const multer = require('multer');
+
 const Product = require('../models/product');
+
+const storage = multer.diskStorage({
+  destination(req, file, callback) {
+    callback(null, './uploads/');
+  },
+  filename(req, file, callback) {
+    callback(null, `${Date.now()}-${file.originalname}`);
+  }
+});
+
+const fileFilter = (req, file, callback) => {
+  const imageMIMETypes = [
+    'image/jpeg',
+    'image/png'
+  ];
+
+  const isValidMIMEType = imageMIMETypes.includes(file.mimetype);
+
+  callback(null, isValidMIMEType);
+};
+
+const upload = multer({ 
+  storage,
+  limits: {
+    fieldSize: 1024 * 1024 * 5,
+  },
+  fileFilter
+});
 
 router.get('/', (req, res, next) => {
   Product
     .find()
-    .select('_id name price')
+    .select('_id name price image')
       .then(products => {
         res.status(200).json({
           message: 'Handling GET request /products',
@@ -20,26 +50,29 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/:id', (req, res, next) => {
-  Product.findById(req.params.id)
-    .then(product => {
-      res.status(200).json({
-        message: 'Handling GET request /products/:id',
-        product
+  Product
+    .findById(req.params.id)
+    .select('_id name price image')
+      .then(product => {
+        res.status(200).json({
+          message: 'Handling GET request /products/:id',
+          product
+        });
+      })
+      .catch(error => {
+        res.status(404).json({
+          error
+        });
       });
-    })
-    .catch(error => {
-      res.status(404).json({
-        error
-      });
-    });
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', upload.single('image'), (req, res, next) => {
   const { name, price } = req.body;
 
   new Product({
     name,
-    price
+    price,
+    image: req.file.path
   })
     .save()
       .then(product => {
